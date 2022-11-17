4a1a79e01a4e52581869f35f47ee12e96756e7cb
ure <ure@moreorless.be>
2022-11-17 10:22:18 +0100

    update

--- code.py
+++ code.py
@@ -10,10 +10,12 @@ from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
 from adafruit_hid.keycode import Keycode
 from adafruit_hid.consumer_control import ConsumerControl
 from adafruit_hid.consumer_control_code import ConsumerControlCode
+from rainbowio import colorwheel
+
 
 async def buttonTask(cc):
     while (True):
-        await asyncio.sleep(0.2)
+        await asyncio.sleep(0.1)
         if µPICO.buttonLeft.value is False:
             print("ButtonLeft Pressed!")
             cc.send(ConsumerControlCode.VOLUME_INCREMENT)
@@ -23,23 +25,23 @@ async def buttonTask(cc):
 
 async def buzzerTask():
     while (True):
-        simpleio.tone(board.GP10, 440, duration=0.5)
+	for f in (262, 294, 330, 349, 392, 440, 494, 523):
+		simpleio.tone(board.GP10, f, duration=0.25)
         await asyncio.sleep(5)
 
-#async def blinkyTask():
-#    while (True):
-#        µPICO.RGBled1.color = (255, 0, 0)
-#        await asyncio.sleep(2)
-#        µPICO.RGBled1.color = (0, 255, 0)
-#        await asyncio.sleep(2)
-#        µPICO.RGBled1.color = (0, 0, 255)
-#        await asyncio.sleep(2)
-#        µPICO.RGBled1.color = (0, 255, 255)
-#        await asyncio.sleep(2)
-#        µPICO.RGBled1.color = (255, 255, 0)
-#        await asyncio.sleep(2)
-#        µPICO.RGBled1.color = (255, 255, 255)
-#        await asyncio.sleep(2)
+async def blinkyTask():
+    while (True):
+	for i in range(255):
+		i = (i + 1) % 256
+		µPICO.RGBled1.color = colorwheel(i)
+		await asyncio.sleep(0.01)
+
+async def blinkyTaskTwo():
+    while (True):
+	for i in range(255):
+		i = (i + 1) % 256
+		µPICO.RGBled2.color = colorwheel(i)
+		await asyncio.sleep(0.01)
 
 async def main():
     print("Intializing µPico")
@@ -54,7 +56,8 @@ async def main():
 
     loop = asyncio.get_event_loop()
     button_task = asyncio.create_task(buttonTask(cc))
-    #blinky_task = asyncio.create_task(blinkyTask())
+    blinky_task = asyncio.create_task(blinkyTask())
+    blinky_task_two = asyncio.create_task(blinkyTaskTwo())
     buzzer_task = asyncio.create_task(buzzerTask())
 
     loop.run_until_complete(button_task)
---
+++ provision.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+DIR="/Volumes/RPI-RP2"
+if [ -d "$DIR" ]; then
+  echo "Installing firmwire to pico in ${DIR}..."
+  cd /tmp
+  wget https://downloads.circuitpython.org/bin/raspberry_pi_pico/en_US/adafruit-circuitpython-raspberry_pi_pico-en_US-7.3.3.uf2
+  cp adafruit-circuitpython-raspberry_pi_pico-en_US-7.3.3.uf2 /Volumes/RPI-RP2
+  echo "Sleeping 10 seconds for firmware to install"
+  cd -
+  sleep 10
+fi
+
+DIR="/Volumes/CIRCUITPY"
+if [ -d "$DIR" ]; then
+  echo "Install demo software in ${DIR}..."
+  cp -r lib /Volumes/CIRCUITPY
+  cp *.py /Volumes/CIRCUITPY
+  diskutil unmount /Volumes/CIRCUITPY
+  echo "done"
+fi
+
+DIR="/Volumes/µPICO"
+if [ -d "$DIR" ]; then
+  echo "Updating demo software in ${DIR}..."
+  cp -r lib /Volumes/CIRCUITPY
+  cp *.py /Volumes/CIRCUITPY
+  echo "done"
+fi
