42000ad9fed371eefe100fd7f78df62dd162c026
ure <ure@moreorless.be>
2023-03-24 13:59:23 +0100

    fix

--- provision.sh
+++ provision.sh
@@ -4,11 +4,11 @@ DIR="/Volumes/RPI-RP2"
 if [ -d "$DIR" ]; then
   echo "Installing firmwire to pico in ${DIR}..."
   cd /tmp
-  wget https://downloads.circuitpython.org/bin/raspberry_pi_pico/en_US/adafruit-circuitpython-raspberry_pi_pico-en_US-7.3.3.uf2
+  wget https://downloads.circuitpython.org/bin/raspberry_pi_pico/en_US/adafruit-circuitpython-raspberry_pi_pico-en_US-8.0.4.uf2
   cp adafruit-circuitpython-raspberry_pi_pico-en_US-8.0.4.uf2 /Volumes/RPI-RP2
-  echo "Sleeping 10 seconds for firmware to install"
+  echo "Sleeping 20 seconds for firmware to install"
   cd -
-  sleep 10
+  sleep 20
 fi
 
 DIR="/Volumes/CIRCUITPY"
