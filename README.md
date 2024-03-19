9821463e6a69c5e619656bcacf4980fb956472f3
ure <ure@moreorless.be>
2024-03-19 11:41:51 +0100

    cleanup

---
+++ README.md
@@ -0,0 +1,13 @@
+# µPico
+
+[PCB](https://shop.rf.guru/products/2023-p-104) µPico PCB
+
+# µPico Board Hardware
+
+in hw/schmatics.pdf you can find the schematics
+in hw/ you can find the bomlist, pick and place and gerber file for the PCB
+in hw/enclosure.stl yuo can find the enclosure
+
+# µPico Board Software
+
+in src/ you can find source and lot's of examples
---
+++ src/provision.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+DIR="/Volumes/RPI-RP2"
+if [ -d "$DIR" ]; then
+  echo "Installing firmwire to pico in ${DIR}..."
+  cd /tmp
+  wget https://downloads.circuitpython.org/bin/raspberry_pi_pico/en_US/adafruit-circuitpython-raspberry_pi_pico-en_US-8.0.4.uf2
+  cp adafruit-circuitpython-raspberry_pi_pico-en_US-8.0.4.uf2 /Volumes/RPI-RP2
+  echo "Sleeping 40 seconds for firmware to install"
+  cd -
+  sleep 40
+fi
+
+DIR="/Volumes/CIRCUITPY"
+if [ -d "$DIR" ]; then
+  echo "Install demo software in ${DIR}..."
+  cp -r lib /Volumes/CIRCUITPY
+  cp *.py /Volumes/CIRCUITPY
+  diskutil unmount /Volumes/CIRCUITPY
+  echo "done"
+fi
+
+DIR="/Volumes/MICROPICO"
+if [ -d "$DIR" ]; then
+  echo "Updating demo software in ${DIR}..."
+  cp -r lib /Volumes/MICROPICO
+  cp *.py /Volumes/MICROPICO
+  echo "done"
+fi
